7aef92c26d9a3bba1015f379412c4f83ff701e37
skn437 <skn437physx@gmail.com>
2024-02-05 12:11:33 +0600

    Added Data Fetching From Spring Boot API

---
+++ src/routes/wang/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { PageServerData } from "./$types";
+	import "./wang.scss";
+	//import { token } from "../layout.svelte";
+
+	let { data } = $props<PagePropsDataType<PageServerData>>();
+
+	console.log(data);
+	//	console.log(token.csrf.token);
+</script>
+
+<template>
+	<p class="message">{JSON.stringify(data.message)}</p>
+	<p class="message">{JSON.stringify(data.message2)}</p>
+</template>
+
+<style lang="scss">
+</style>
